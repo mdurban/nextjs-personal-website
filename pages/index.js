83eeef63132fce821eda04e8f5ad557a1e6bf501
mdurban <mike.david.urban@gmail.com>
2020-05-09 11:45:08 -0400

    remove default Nextjs home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-import Link from 'next/link'
-
+import styles from './home.module.css'
 
 export default function Home() {
   return (
@@ -9,57 +8,97 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={styles.heroContainer}>
+        <div className={styles.heroText}>
+          <h1 className={styles.heroTitle}>
+            Michael Urban
+          </h1>
+          {/* <div className={styles.heroTitle}>Making money easy</div> */}
+          <div className={styles.heroSubtitle}>
+            After robots take over the world, our code will be the last evidence of human existence.
+          </div>
+          <br />
+          <div className={styles.heroSubtitle}>
+            <span className={styles.descriptionLeader}> So it better be good.</span>
+          </div>
+        </div>
+      </div>
 
       <main>
-        <h1 className="title">
-          Learn to <a href="https://nextjs.org">Next.js!</a>
-          <Link href='/home'>Home</Link>
-        </h1>
+        <div>
+          Take a look at some of my work.
+        </div>
+        <div className={styles.projects}>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <div className={styles.projectContainer}>
+            <div className={styles.projectDescription}>
+              <div className={styles.projectTitle}>MoneyForNoobs</div>
+              <div className={styles.projectSubTitle}>When I tried learning the basics of finances, I was frustrated at how confusing it was. I had a difficult time finding any blog posts or podcasts aimed at people who knew nothing and I found myself lost in sea of cryptic mathematical equations and unfamiliar lingo. The basics of money is something that I feel everyone should know, yet it feels so hard to learn.</div>
+              <div className={styles.projectSubTitle}>My goal is to create a blog where people can start learning the basics in an accessible way. I want to have a place where people who know nothing about finances will feel welcome and not overwhelmed.</div>
+              <div className={styles.projectSubTitle}><span className={styles.descriptionLeader}>Tech Stack: </span>React, Redux, SASS, Gatsby</div>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            </div>
+            <div className={styles.projectImages} >
+              <a href='https://www.moneyfornoobs.com' target='_blank'>
+                <img className={styles.workImage} src="/mfn-desktop-full.png" alt="Vercel Logo" />
+              </a>
+            </div>
+          </div>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <div className={styles.projectContainer}>
+            <div className={styles.projectImages}>
+              <img className={styles.workImage} src="/oil-map.png" alt="Vercel Logo" />
+            </div>
+            <div className={styles.projectDescription}>
+              <div className={styles.projectTitle}>Oil Well Efficiency Dashboard</div>
+              <div className={styles.projectSubTitle}>Data visualization dashboard that helps client view status, efficiency and operating cost of gas and oil wells across the country</div>
+              <div className={styles.projectSubTitle}><span className={styles.descriptionLeader}>Tech Stack: </span>React, Redux, MaterialUI</div>
+            </div>
+          </div>
 
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          <div className={styles.projectContainer}>
+            <div className={styles.projectDescription}>
+              <div className={styles.projectTitle}>Representatives Finder Website</div>
+              <div className={styles.projectSubTitle}>Website that helps users search for their representatives based on their location. Gives information for city level officials up to federal.</div>
+              <div className={styles.projectSubTitle}><span className={styles.descriptionLeader}>Tech Stack: </span>React, Redux, SASS</div>
+            </div>
+            <div className={styles.projectImages}>
+              <a href='https://luiscarlin.github.io/representatives/' target='_blank'>
+                <img className={styles.workImage} src="/reps.png" alt="Vercel Logo" />
+              </a>
+            </div>
+          </div>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={styles.projectContainer}>
+            <div className={styles.sideBySideImages}>
+              <img className={styles.singleImage} src="/prototype-1.png" alt="Vercel Logo" />
+              <img className={styles.singleImage} src="/prototype-2.png" alt="Vercel Logo" />
+            </div>
+            <div className={styles.projectDescription}>
+              <div className={styles.projectTitle}>Real Estate Auction Prototype</div>
+              <div className={styles.projectSubTitle}>Helped develop mobile application prototype that secured first round of funding for real estate startup.</div>
+              <div className={styles.projectSubTitle}>The prototype was used by the company to show investors what functions they were planning on developing.</div>
+              <div className={styles.projectSubTitle}>It hooked into an in-house cryptocurrency application to allow users to bid on real estate property using digital currency.</div>
+              <div className={styles.projectSubTitle}><span className={styles.descriptionLeader}>Tech Stack: </span>React, Redux, MaterialUI, styled components</div>
+            </div>
+          </div>
+
+
         </div>
       </main>
 
       <footer>
-        <a
+        <div
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+          <div>Website handcrafted by <span className={styles.footerHighlight}>me</span> specifically for <span className={styles.footerHighlight}>you</span>.</div>
+          <div>Built using React and Next.js</div>
+          <div>
+            Find my code on <a className={styles.iconLink} href='https://www.github.com/mdurban' target='_blank'><img src="/github-logo.png" alt="Github Logo" className="logo" /></a>
+          </div>
+        </div>
       </footer>
 
       <style jsx>{`
@@ -84,7 +123,7 @@ export default function Home() {
         footer {
           width: 100%;
           height: 100px;
-          border-top: 1px solid #eaeaea;
+          border-top: 1px solid #e2e2e2;
           display: flex;
           justify-content: center;
           align-items: center;
@@ -158,7 +197,7 @@ export default function Home() {
           text-align: left;
           color: inherit;
           text-decoration: none;
-          border: 1px solid #eaeaea;
+          border: 1px solid #e2e2e2;
           border-radius: 10px;
           transition: color 0.15s ease, border-color 0.15s ease;
         }
@@ -207,6 +246,6 @@ export default function Home() {
           box-sizing: border-box;
         }
       `}</style>
-    </div>
+    </div >
   )
 }
